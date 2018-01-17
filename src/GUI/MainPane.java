5917ce3ba30b127db6c6db83f4dab06c6bf942b3
Raffaele Mignone <raffy.m.96@gmail.com>
2018-01-17 21:17:50 +0100

    0.1 - beta

--- src/GUI/MainPane.java
+++ src/GUI/MainPane.java
@@ -1,3 +1,22 @@
+/*
+ *       copyright (c)  2018 Raffaele Mignone
+ *
+ *        This file is part of D Client
+ *
+ *        D Client is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU General Public License as published by
+ *        the Free Software Foundation, either version 3 of the License, or
+ *        (at your option) any later version.
+ *
+ *        D Client is distributed in the hope that it will be useful,
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *        GNU General Public License for more details.
+ *
+ *        You should have received a copy of the GNU General Public License
+ *        along with D Client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package GUI;
 
 /*
@@ -61,7 +80,7 @@ public class MainPane {
         });
 
         okButton.addActionListener(actionEvent -> {
-            int status = 200;
+            int status = 202;
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
             String name = nameTF.getText();
 
@@ -86,11 +105,13 @@ public class MainPane {
                 status = e.getStatus().getCode();
             }
 
-            if(status==403)
+            if(status == 202)
+                JOptionPane.showMessageDialog(null, "Evento creato con successo");
+            else if(status == 403)
                 JOptionPane.showMessageDialog(null, "User name o password sbagliata");
-            else if(status==404)
+            else if(status == 404)
                 JOptionPane.showMessageDialog(null, "Devi specificare un nome per l'evento");
-            else if(status==409)
+            else if(status == 409)
                 JOptionPane.showMessageDialog(null, "L'evento già esiste");
         });
     }
